058d735d26eecd8a2a56b209a2a25c7861a333e8
The Joel <jotelfootball@gmail.com>
2026-03-13 18:12:51 +0000

    update

--- components/Loader.tsx
+++ components/Loader.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Loader() {
   return (
     <div className="page-loader" id="loader">
-      <div className="loader-text">IVANN</div>
+      <div className="loader-text">JOEL</div>
     </div>
   );
 }
